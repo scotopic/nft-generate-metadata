943b763b87714935fe1a4270cae3a92c6a7dfbd7
Alex G <hi+github@mavenblueprint.com>
2022-08-11 21:23:50 -0400

    Add a readme, usage and general purpose Chia CHIP-0007 metadata generation script

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# nft-generate-metadata
+# Generate Chia NFT metadata
+
+## Usage
+
+Generate UUID:
+`python metadata.py -gu`
+
+Generate a single NFT metadata file:
+`python metadata.py -gm "1" "Friendly Marmots #0001" "Eco friendly avatars" '{"Head":"yes", "Body":"wow", "Eyes":3}'`
+
+## Usage
+
+1. Generate the your collection UUID (do this only one time per NFT collection): `python metadata.py -gu`
+1. Open `metadata.py` and replace the your specific collection related info
+1. Generate a single metadata file with any attributes: `python metadata.py -gm "1" "Friendly Marmots #0001" "Eco friendly avatars" '{"Head":"yes", "Body":"wow", "Eyes":3}'`
+1. Copy/paste the output to [https://www.jsonschemavalidator.net/s/0Aw7Bmlb](https://www.jsonschemavalidator.net/s/0Aw7Bmlb) to validate ouput.
+1. You are now ready to upload to IPFS/Arweave.
+
+## Requirements
+
+Python 3.6+
---
+++ metadata.py
@@ -0,0 +1,146 @@
+import json
+import asyncio
+import os
+import argparse
+import sys
+import uuid
+
+##############################
+#### REPLACE THESE VALUES ####
+##############################
+NFT_COLLECTION_UUID="Run <python metadata.py -gu>"
+NFT_COLLECTION_NAME="Collection Name"
+NFT_COLLECTION_DESCRIPTION="Example collection of 10000 unique marmots."
+NFT_COLLECTION_ICON_URI="https://nftexample.com/assets/img/thumbnail.png"
+NFT_COLLECTION_BANNER_URI="https://nftexample.com/assets/img/banner.png"
+NFT_COLLECTION_TWITTER_HANDLE="@NFTexample"
+NFT_COLLECTION_WEBSITE_URI="https://nftexample.com"
+##############################
+#### REPLACE THESE VALUES ####
+##############################
+
+# Replace if needed
+OUTPUT_DIR="_output"
+
+def gen_chia_chip_0007_attributes_from_json(key_value_json):
+    chip0007_formatted_json = []
+    
+    try:
+        # load the JSON string
+        json_data = json.loads(key_value_json)
+    except Exception as e:
+        print(e)
+        sys.exit(f"ERROR parsing JSON: {key_value_json}")
+    
+    for attribute in json_data:
+        # print(attribute + ":" + json_data[attribute])
+        key = attribute
+        value = json_data[attribute]
+        json_attribute = {
+          "trait_type": f"{key}",
+          "value": f"{value}"
+        }
+        chip0007_formatted_json.append(json_attribute)
+    
+    return chip0007_formatted_json
+
+def gen_nft_metadata(nft_number, nft_name, nft_description, first_trait_value):
+    
+    meta = {
+      "format": "CHIP-0007",
+      "name": f"{nft_name}",
+      "description": f"{nft_description}",
+      "sensitive_content": False,
+      "attributes": [
+        {
+          "trait_type": "Generation",
+          "value": 1
+        }
+      ],
+      "collection": {
+        "name": f"{NFT_COLLECTION_NAME}",
+        "id": f"{NFT_COLLECTION_UUID}",
+        "attributes": [
+          {
+            "type": "description",
+            "value": f"{NFT_COLLECTION_DESCRIPTION}"
+          },
+          {
+              "type": "icon",
+              "value": f"{NFT_COLLECTION_ICON_URI}"
+          },
+          {
+              "type": "banner",
+              "value": f"{NFT_COLLECTION_BANNER_URI}"
+          },
+          {
+            "type": "twitter",
+            "value": f"{NFT_COLLECTION_TWITTER_HANDLE}"
+          },
+          {
+            "type": "website",
+            "value": f"{NFT_COLLECTION_WEBSITE_URI}"
+          }
+        ]
+      }
+    }
+    
+    extra_attributes = gen_chia_chip_0007_attributes_from_json(first_trait_value)
+    
+    if len(extra_attributes) > 0:
+        for attr in extra_attributes:
+            meta["attributes"].append(attr)
+    
+    fname = f"{nft_number}.json"
+    
+    try:
+        if os.path.exists(OUTPUT_DIR) != True:
+            os.mkdir(OUTPUT_DIR)
+    except Exception as e:
+        print(e)
+        sys.exit(f"ERROR creating {OUTPUT_DIR}")
+    
+    try:
+        with open(f"{OUTPUT_DIR}/" + fname, 'w') as outfile:
+            json.dump(meta, outfile, sort_keys=False, indent=4)
+    except Exception as e:
+        print(e)
+        sys.exit(f"ERROR writing out {fname}")
+        
+
+def gen_nft_metadata_validate(nft_metadata_path):
+    # https://www.jsonschemavalidator.net/s/0Aw7Bmlb
+    print("do this")
+
+def generate_uuid():
+    print(str(uuid.uuid4()).upper())
+
+def get_args():
+    
+    parser = argparse.ArgumentParser(description='Generate valid Chia CHIP-0007 metadata.')
+    
+    parser.add_argument('-gu', '--generate-uuid', action='store_true', required=False, help='Generate a new Chia NFT collection UUID.')
+    parser.add_argument('-gm', '--generate-metadata', metavar=('NFT_NUMBER', 'NFT_NAME', 'NFT_DESCRIPTION', 'TRAITS_AS_KEY_VALUE_JSON'), nargs=4, required=False, help='Generate NFT metadata.\nExample: python metadata.py -gm "0001" "Eco Friends #0001" "The one that got away" "{"Body":"K33", "Eyes":3, "Mouth":"Nasty"}"')
+    
+    if len(sys.argv) < 2:
+        # parser.print_usage()
+        parser.print_help()
+        sys.exit(1)
+    
+    return parser.parse_args()
+
+ARGS = get_args()
+
+async def main():
+    
+    if ARGS.generate_metadata:
+        nft_number = ARGS.generate_metadata[0]
+        nft_name = ARGS.generate_metadata[1]
+        nft_description = ARGS.generate_metadata[2]
+        traits_as_key_values_json_string = ARGS.generate_metadata[3]
+        gen_nft_metadata(nft_number, nft_name, nft_description, traits_as_key_values_json_string)
+    elif ARGS.generate_uuid:
+        generate_uuid()
+    
+
+asyncio.run(main())
